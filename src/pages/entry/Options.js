857dc42bdde85f0a3ba458c84ebe650c6b647318
Jaggehn <jaggehns@gmail.com>
2021-08-31 03:16:19 +0800

    Mock Service Worker tests

---
+++ src/pages/entry/Options.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import ScoopOption from "./ScoopOption";
+import Row from "react-bootstrap/Row";
+import ToppingOption from "./ToppingOption";
+import AlertBanner from "../common/AlertBanner";
+
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
+  //optionType is 'scoops' or 'toppings'
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => setError(true));
+  }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
+  //TODO: replace 'null' with ToppingOption
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
+}
---
+++ src/pages/entry/ToppingOption.js
@@ -0,0 +1,14 @@
+import Col from "react-bootstrap/Col";
+
+export default function ToppingOption({ name, imagePath }) {
+  return (
+    //col is 12 divided by to know how many rows
+    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
+      <img
+        style={{ width: "75%" }}
+        src={`http://localhost:3030/${imagePath}`}
+        alt={`${name} topping`}
+      />
+    </Col>
+  );
+}
